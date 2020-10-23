96c8890b0095ed8afa556352a564666f0992bb59
Михаил Пискунов <57969817+piskunovma@users.noreply.github.com>
2020-10-23 13:36:16 +0300

    update Lesson_5 (#6)

--- mainapp/migrations/0001_initial.py
+++ mainapp/migrations/0001_initial.py
@@ -8,29 +8,31 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='ProductCategory',
+            name="ProductCategory",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=64, unique=True, verbose_name='имя')),
-                ('description', models.TextField(blank=True, verbose_name='описание')),
+                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("name", models.CharField(max_length=64, unique=True, verbose_name="имя")),
+                ("description", models.TextField(blank=True, verbose_name="описание")),
             ],
         ),
         migrations.CreateModel(
-            name='Product',
+            name="Product",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=128, verbose_name='имя продукта')),
-                ('image', models.ImageField(blank=True, upload_to='products_images')),
-                ('short_desc', models.CharField(blank=True, max_length=60, verbose_name='краткое описание продукта')),
-                ('description', models.TextField(blank=True, verbose_name='описание продукта')),
-                ('price', models.DecimalField(decimal_places=2, default=0, max_digits=8, verbose_name='цена продукта')),
-                ('quantity', models.PositiveIntegerField(default=0, verbose_name='количество на складе')),
-                ('category', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='mainapp.productcategory')),
+                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("name", models.CharField(max_length=128, verbose_name="имя продукта")),
+                ("image", models.ImageField(blank=True, upload_to="products_images")),
+                ("short_desc", models.CharField(blank=True, max_length=60, verbose_name="краткое описание продукта")),
+                ("description", models.TextField(blank=True, verbose_name="описание продукта")),
+                ("price", models.DecimalField(decimal_places=2, default=0, max_digits=8, verbose_name="цена продукта")),
+                ("quantity", models.PositiveIntegerField(default=0, verbose_name="количество на складе")),
+                (
+                    "category",
+                    models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="mainapp.productcategory"),
+                ),
             ],
         ),
     ]
