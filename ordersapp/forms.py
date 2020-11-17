0d8fb707b5a7cb591c0b44e1810f050030135f2d
Михаил Пискунов <piskunov.m.a@gmail.com>
2020-11-17 17:26:35 +0300

    add les2_task4

--- ordersapp/forms.py
+++ ordersapp/forms.py
@@ -15,10 +15,16 @@ class OrderForm(forms.ModelForm):
 
 
 class OrderItemForm(forms.ModelForm):
+    price = forms.CharField(label="цена", required=False)
+
     def __init__(self, *args, **kwargs):
         super(OrderItemForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "form-control"
+
+    class Meta:
+        model = OrderItem
+        exclude = ()
 
     class Meta:
         model = OrderItem
